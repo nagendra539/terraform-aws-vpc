07f2d34c10782c0cf8c7841209540e1d212d4963
Nagendra <143023628+nagendra539@users.noreply.github.com>
2024-10-03 23:12:08 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -17,5 +17,5 @@ output "database_subnet_ids" {
   value = aws_subnet.database[*].id
 }
 output "database_subnet_group_name"{
-  value = aws_db_subnet_group.default.name
+  value = aws_db_subnet_group.db_subnet_group.name
 }
