89ddc6fcea17e85bd7c3121c66aada905b24ff95
Nagendra <143023628+nagendra539@users.noreply.github.com>
2024-10-03 15:33:27 +0530

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,4 +1,4 @@
 locals {
-  resource_name = "${var.Project_name}-${var.environment}"
+  resource_name = "${var.project_name}-${var.environment}"
   az_names = slice(data.aws_availability_zones.available.names,0,2) # we are selecting the first two AZ's using slice.
 }
