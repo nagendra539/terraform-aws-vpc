378fcc9eacec84f24cd1756e2f3674ca84ef8af5
Nagendra <143023628+nagendra539@users.noreply.github.com>
2024-10-03 22:55:53 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,12 @@ output "igw_id" {
 output "az_info" {
   value = data.aws_availability_zones.available
 }
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
+output "database_subnet_ids" {
+  value = aws_subnet.database[*].id
+}
